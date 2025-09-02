97533bf46911b1fd69ab62afdfde61f89556ad1a
Ernie_Velazquez <ernie.velazquez@live.com>
2025-09-01 19:13:19 -0700

    added pygame library and launched initial game window

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from constants import (  # noqa: F401
-    ASTEROID_KINDS,
-    ASTEROID_MAX_RADIUS,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_SPAWN_RATE,
-    SCREEN_HEIGHT,
-    SCREEN_WIDTH,
-)
+import pygame
+
+from constants import *
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+
 
 
 if __name__ == "__main__":
