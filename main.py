ad09c3fb12cdbc2c13295a97a86d955924257f5c
Ernie_Velazquez <ernie.velazquez@live.com>
2025-09-01 19:16:16 -0700

    moved event handler within infinite loop

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-
     while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black")
         pygame.display.flip()
 
