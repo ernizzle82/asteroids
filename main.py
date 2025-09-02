870364cb8b36d54d2aac8a1b9ddb43adfc1f0113
Ernie_Velazquez <ernie.velazquez@live.com>
2025-09-01 18:33:30 -0700

    added constants module

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from constants import (  # noqa: F401
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_SPAWN_RATE,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+
+
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
