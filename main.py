ec9ed1175de0e1a4c66084152bf70167f139e23c
Ernie_Velazquez <ernie.velazquez@live.com>
2025-09-01 19:23:28 -0700

    added time clock object and delta time variable to limit framerate

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -16,9 +18,8 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
-
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
